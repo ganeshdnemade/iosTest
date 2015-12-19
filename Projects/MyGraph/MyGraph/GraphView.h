ce378f063e3873b35abff1f662de2138b9b3ea96
mac <mac@macs-MacBook-Pro-4.local>
2015-12-20 05:14:25 +0530

    bar chart finished

--- Projects/MyGraph/MyGraph/GraphView.h
+++ Projects/MyGraph/MyGraph/GraphView.h
@@ -11,18 +11,22 @@
 #define kGraphHeight 300
 #define kDefaultGraphWidth 900
 #define kOffsetX 10
-#define kStepX 50
+#define kStepX 70
 #define kGraphBottom 300
 #define kGraphTop 0
 
 
-#define kStepY 50
+#define kStepY 70
 #define kOffsetY 10
 #define kGraphLeft 10
 
+#define kBarTop 10
+#define kBarWidth 40
+ 
 
 
 @interface GraphView : UIView
+-(void)drawBar:(CGRect)rect context:(CGContextRef)ctx;
 
 
 
--- Projects/MyGraph/MyGraph/GraphView.m
+++ Projects/MyGraph/MyGraph/GraphView.m
@@ -7,9 +7,54 @@
 //
 
 #import "GraphView.h"
-
 @implementation GraphView
 
+    float data[] = {0.7,0.4,0.9,1.0,0.2, 0.85, 0.11, 0.75, 0.53, 0.44, 0.88 , 0.77};
+
+
+
+-(void)drawBar:(CGRect)rect context:(CGContextRef)ctx{
+    
+    CGFloat components[12] = {0.2314,0.5686, 0.4, 1.0,
+        0.4727, 1.0,0.8, 1.0,
+        0.23, 0.5,8.3,1.0};
+    
+    CGFloat locations[3] = {0.0, 0.33, 1.0};
+    
+    size_t num_locations = 3;
+    
+    
+    CGColorSpaceRef colorspace = CGColorSpaceCreateDeviceRGB();
+    CGGradientRef gradient = CGGradientCreateWithColorComponents(colorspace, components, locations, num_locations);
+    CGPoint startPoint = rect.origin;
+    CGPoint endPoint = CGPointMake(rect.origin.x + rect.size.width, rect.origin.y);
+    
+ 
+
+    
+    CGContextBeginPath(ctx);
+    CGContextSetGrayFillColor(ctx, 0.2,0.7);
+    CGContextMoveToPoint(ctx, CGRectGetMinX(rect), CGRectGetMinY(rect));
+    CGContextAddLineToPoint(ctx, CGRectGetMaxX(rect), CGRectGetMinY(rect));
+    CGContextAddLineToPoint(ctx, CGRectGetMaxX(rect), CGRectGetMaxY(rect));
+    CGContextAddLineToPoint(ctx, CGRectGetMinX(rect), CGRectGetMaxY(rect));
+    CGContextClosePath(ctx);
+ 
+    
+    CGContextSaveGState(ctx);
+    CGContextClip(ctx);
+    
+    CGContextDrawLinearGradient(ctx, gradient, startPoint, endPoint, 0);
+    CGContextRestoreGState(ctx);
+    
+    //Release the resources
+    CGColorSpaceRelease(colorspace);
+    CGGradientRelease(gradient);
+    
+    
+    
+    
+  }
 
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
@@ -22,15 +67,16 @@
     
     //How many lines?
     int howMany = (kDefaultGraphWidth - kOffsetX)/kStepX;
-    
+    CGFloat dash[] = {2.0, 2.0};
+    CGContextSetLineDash(context, 0.0, dash,2);
 //    //here the lines go
     for (int i=0; i<=howMany; i++) {
         
         CGContextMoveToPoint(context, kOffsetX + i*kStepX, kGraphTop);
         CGContextAddLineToPoint(context, kOffsetX + i*kStepX, kGraphBottom);
     }
-    
-    
+    CGContextStrokePath(context);
+    CGContextSetLineDash(context, 0, NULL,0);
     int howManyHorizontal = (kGraphHeight - kOffsetY)/kStepY;
     
     for (int i=0; i<=howManyHorizontal; i++){
@@ -39,7 +85,31 @@
     }
     
   
-     CGContextStrokePath(context);
+    
+    
+    CGContextStrokePath(context);
+    
+    
+    
+    float maxBarHeight = kGraphHeight - kBarTop - kOffsetY;
+    
+    for (int i =0;i<sizeof(data); i++) {
+        float barX =  kOffsetX + kStepX + i*kStepX - kBarWidth/2;
+        float barY = kBarTop + maxBarHeight - maxBarHeight * data[i];
+        float barHeight = maxBarHeight * data[i];
+        
+        CGRect barRect = CGRectMake(barX, barY, kBarWidth, barHeight);
+        [self drawBar:barRect context:context];
+        
+    }
+
+    
+//setting background to graph
+//    UIImage *image = [UIImage imageNamed:@"mario.jpg"];
+//    CGRect imageRect = CGRectMake(0,0, image.size.width, image.size.height);
+//    CGContextDrawImage(context, imageRect, image.CGImage);
+    
+                     
     
 }
 
